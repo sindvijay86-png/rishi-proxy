161341a37683e3687410330ef7b7a2880a3cf8fc
sindvijay86-png <sindvijay86@gmail.com>
2026-06-09 14:29:09 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   const response = await fetch("https://api.anthropic.com/v1/messages", {
     method: "POST",
     headers: {
-    "x-api-key":"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" , 
+    "x-api-key":"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" , 
       "anthropic-version": "2023-06-01",
     },
     body: JSON.stringify(req.body),
